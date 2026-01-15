3356c6c226ee18947b16b818cfc473614700226a
loksuf <zay.mir2008@gmail.com>
2026-01-15 13:36:22 +0300

    Upd eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,14 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": "warn"
+    },
+    languageOptions: {
+      globals: { ...globals.browser }
+    } 
+  },
 ]);
